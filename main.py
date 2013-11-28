b328ab2b90bc16b36665dc71f6dc7e0fe05d69ba
Eric Hansen <ehansen@anzensolutions.com>
2013-11-28 13:56:17 -0500

    Fun stuff

--- main.py
+++ main.py
@@ -19,15 +19,14 @@ handlers = [
     ]
 
 plugins = systemp.load_plugins()
-print "Plugins loaded:",plugins
+
 db = sqlite.connect('collection.db')
 
 cur = db.cursor()
 
 opts = {}
 
-print "looping plugins"
-for cls,dat in systemp.web_plugins().iteritems():
+for cls, dat in systemp.web_plugins().iteritems():
     ref = dat['ref']
 
     if hasattr(ref, 'get_opts'):
@@ -38,7 +37,6 @@ for cls,dat in systemp.web_plugins().iteritems():
         handlers.append((
             ref.PATH, ref, opts
             ))
-print "done."
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -49,12 +47,15 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Boobs")
 
-print "Handlers:",handlers
 app = tornado.web.Application(handlers, **settings)
 
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        pass
 
     db.close()
